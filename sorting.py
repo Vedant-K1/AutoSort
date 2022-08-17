8bcbf9d5f814c15febe18123c8d4e000ac5bbc4b
Vedant <kamblivedant50@gmal.com>
2022-08-17 13:00:35 +0530

    Create sorting.py

---
+++ sorting.py
@@ -0,0 +1,12 @@
+#create a function to sort images and videos which are uploaded:
+#TODO based on creation date
+#TODO create a separate folder for each trip
+#TODO One day, one subfolder in trip's folder
+#TODO Default: Unsorted folder
+#TODO Quick GUI for putting unsorted images into a folder/subfolder
+#TODO Dockerize?
+#TODO Combine with android app??
+
+
+def sort_fldr_img(folder_name,trip,default='Unsorted'):
+
